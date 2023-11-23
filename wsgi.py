747ca28e7162246c564b0922e14e48eced24060d
Raghav Vinayak Dadhich <72245900+rvd-24@users.noreply.github.com>
2023-11-23 17:58:25 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,6 @@ import os, sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from index import app
+from app import app
 if __name__ == "__main__":
     app.run()
